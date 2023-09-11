b47dcccce5581aace706f6698448bae2255ff7f2
asbdu962k22us217 <144606642+asbdu962k22us217@users.noreply.github.com>
2023-09-11 13:30:02 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+def recur_fact(x):
+  if x==1:
+    return 1
+  else:
+    return(x* recur_fact(x-1))
+num=int(input("Enter a number:")) 
+if num >=1:
+     print("The factorial of",num,"is",recur_fact(num))
